48926134756a546bcd70565b8fdcbc42468b02f2
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-06-22 20:34:24 +0700

    finish custom_bottom_navigation_bar

---
+++ lib/components/custom_bottom_navigation_bar.dart
@@ -0,0 +1,66 @@
+import 'package:beauty_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+      ),
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            decoration: BoxDecoration(
+              color: kBlackButtonColor,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  'Explore',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.calendar_today),
+            color: kBlackButtonColor,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border),
+            color: kBlackButtonColor,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.widgets,
+              color: kBlackButtonColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:beauty_app/components/custom_bottom_navigation_bar.dart';
 import 'package:beauty_app/constants.dart';
 import 'package:beauty_app/screens/home/components/body.dart';
 import 'package:flutter/material.dart';
@@ -7,41 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Body(),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.only(
-          left: kDefaultPadding * 2,
-          right: kDefaultPadding * 2,
-          bottom: kDefaultPadding / 2,
-        ),
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-              decoration: BoxDecoration(
-                color: kBlackButtonColor,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    'Explore',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+      bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
 }
